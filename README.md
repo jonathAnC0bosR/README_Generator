183cf521cf7a8aba5820bc22f6eb0ce26a87a8e1
Jonathan Cobos <jonathan.cobos@outlook.es>
2023-07-19 23:00:10 -0600

    Made final details, now its working

---
+++ README.md
@@ -0,0 +1,41 @@
+# adfkadfkl
+
+## Table of contents
+
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Licence](#licence)
+- [Contributing](#contributing)
+- [Test](#test)
+- [Questions](#questions)
+
+## Description
+
+adfjaskdflasd.
+
+## Installation
+
+adjfaksdfa.
+
+## Usage 
+
+a.
+
+## Licence
+
+This repository is licenced under the MIT licence.
+
+## Contributing
+
+a
+
+## Test Instructions
+
+a
+
+## Questions
+
+To check my repositories visit: https://github.com/jonathAnC0bosR
+You can contact me on jonathan.cobos@outlook.es.
+
--- index.js
+++ index.js
@@ -37,29 +37,38 @@ inquirer
         },
         {
             type: 'input',
-            message: 'Enter the test information: ',
+            message: 'Enter the test instructions: ',
             name: 'testInfo',
         },
         {
             type: 'input',
-            message: 'Enter the questions content: ',
-            name: 'questions',
+            message: 'Enter your github username: ',
+            name: 'username',
+        },
+        {
+            type: 'input', 
+            message: 'Enter your email: ',
+            name: 'email',
         },
     ]).then((userInput) => {
-        const answers = writeReadme(userInput.projectName, userInput.projectDescription, userInput.installationInfo, userInput.usageInfo, userInput.licence, userInput.contributingInfo, userInput.testInfo, userInput.questions);
-        fs.writeFile('README.md', answers, (err) => {
-            err ? console.log(err) : console.log('README generated succesfully');
+        const answers = writeReadme(userInput.projectName, userInput.projectDescription, userInput.installationInfo, userInput.usageInfo, userInput.licence, userInput.contributingInfo, userInput.testInfo, userInput.username, userInput.email);
+        fs.writeFile('sample-README.md', answers, (err) => {
+            err ? console.log(err) : console.log('\n\n README generated succesfully');
         })
     })
 
-const writeReadme = (projectName, projectDescription, installationInfo, usageInfo, licence, contributingInfo, testInfo, questions) => 
+const writeReadme = (projectName, projectDescription, installationInfo, usageInfo, licence = null, contributingInfo, testInfo, username, email) => 
 `# ${projectName}
 
 ## Table of contents
 
--[Description](<a>Description</a>)
--[Description][]
--[][]
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Licence](#licence)
+- [Contributing](#contributing)
+- [Test](#test)
+- [Questions](#questions)
 
 ## Description
 
@@ -81,13 +90,15 @@ This repository is licenced under the ${licence} licence.
 
 ${contributingInfo}
 
-## Test 
+## Test Instructions
 
 ${testInfo}
 
 ## Questions
 
-${questions}
+To check my repositories visit: https://github.com/${username}
+You can contact me on ${email}.
+
 `
 ;
 
